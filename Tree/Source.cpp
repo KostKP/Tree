e32673058e7571c30358cc40578be57df61c51cb
KostKP <KostKP@HOME-PC>
2021-05-31 10:48:17 +0700

    Добавьте файлы проекта.

---
+++ Tree/Source.cpp
@@ -0,0 +1,235 @@
+#include <Windows.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct Node {
+	int value;
+	Node* next;
+};
+
+void addToList(struct Node* List, int X) {
+	Node* current = new Node;
+	current->value = X;
+	current->next = nullptr;
+	int size = List->value + 1;
+	List->value = List->value + 1;
+	for (int i = 1; i < size; ++i) List = List->next;
+	List->next = current;
+}
+
+Node* createList() {
+	Node* current = new Node;
+	current->value = 0;
+	current->next = nullptr;
+	return current;
+}
+
+int getByIndex(struct Node* List, int x) {
+	for (int i = 0; i <= x - 1; ++i) List = List->next;
+	return List->value;
+}
+
+void showList(struct Node* List) {
+	int size = List->value;
+	List = List->next;
+	cout << "Лист(" << size << "):";
+	for (int i = 0; i < size; ++i) {
+		cout << ' ' << List->value;
+		List = List->next;
+	}
+	cout << endl;
+}
+
+void deleteElement(struct Node* List, int index) {
+	List->value = List->value - 1;
+	for (int i = 1; i < index; ++i) {
+		List = List->next;
+	}
+	Node* TempList = List;
+	List = List->next;
+	TempList->next = List->next;
+	delete List;
+}
+
+int findFirst(struct Node* List, int x) {
+	int size = List->value;
+	List = List->next;
+	for (int i = 1; i <= size; ++i) {
+		if (List->value == x) return i;
+		List = List->next;
+	}
+	return 0;
+}
+
+void sortList(struct Node* List) {
+	Node* TempList1, * TempList2;
+	bool Sorted = false;
+	int size = List->value;
+	while (!Sorted) {
+		Sorted = true;
+		TempList1 = List;
+		for (int i = 1; i < size; ++i) {
+			TempList1 = TempList1->next;
+			TempList2 = TempList1->next;
+			if (TempList1->value > TempList2->value) {
+				swap(TempList1->value, TempList2->value);
+				Sorted = false;
+			}
+		}
+	}
+}
+
+void placePos(struct Node* List, int x) {
+	Node* TempList, * current = new Node;
+	current->value = x;
+	int size = List->value + 1;
+	List->value = size;
+	TempList = List;
+	List = List->next;
+	for (int i = 1; i < size && List->value < x; ++i) {
+		TempList = List;
+		List = List->next;
+	}
+	TempList->next = current;
+	current->next = List;
+}
+
+void main() {
+	srand(time(NULL));
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int size = 0, temp = -1;
+	Node* List = createList();
+
+	while (temp != '8') {
+		showList(List);
+		cout << "1|Добавить элемент(Учёт предыдущих)" << endl;
+		cout << "2|Добавить элемент" << endl;
+		cout << "3|Удалить элемент (Номер)" << endl;
+		cout << "4|Удалить элемент (Элемент)" << endl;
+		cout << "5|Найти индекс" << endl;
+		cout << "6|Сортировка" << endl;
+		cout << "7|Очистка" << endl;
+		cout << "8|Выход" << endl;
+		cout << "Нажмите клавишу";
+		temp = _getch();
+		system("cls");
+		showList(List);
+
+		switch (temp)
+		{
+		case '1': //Добавить элемент
+			cout << "Введите элемент=";
+			cin >> temp;
+			if (findFirst(List, temp)) {
+				cout << "Элемент не добавлен!" << endl;
+				cout << "Нажмите Любую клавишу...";
+				_getch();
+				system("cls");
+				temp = 0;
+				break;
+			}
+			placePos(List, temp);
+			cout << "Элемент " << temp << " добавлен!" << endl;
+			cout << "Нажмите Любую клавишу...";
+			_getch();
+			system("cls");
+			temp = 0;
+			break;
+		case '2': //Добавить элемент в конец
+			cout << "Введите элемент=";
+			cin >> temp;
+			if (findFirst(List, temp)) {
+				cout << "Элемент не добавлен!" << endl;
+				cout << "Нажмите Любую клавишу...";
+				_getch();
+				system("cls");
+				temp = 0;
+				break;
+			}
+			addToList(List, temp);
+			cout << "Элемент " << temp << " добавлен!" << endl;
+			cout << "Нажмите Любую клавишу...";
+			_getch();
+			system("cls");
+			temp = 0;
+			break;
+		case '3': //Удалить по номеру
+			if (List->value == 0) {
+				cout << "Список чист!" << endl << "Нажмите Любую клавишу...";
+				_getch();
+				system("cls");
+				break;
+			}
+			cout << "Введите номер элемента (1-" << List->value << ")=";
+			cin >> temp;
+			if (temp < 1 || temp > List->value) {
+				cout << "Вне диапазона!" << endl;
+				cout << "Нажмите Любую клавишу...";
+				_getch();;
+				system("cls");
+				break;
+			}
+			deleteElement(List, temp);
+			cout << "Элемент удалён!" << endl;
+			cout << "Нажмите Любую клавишу...";
+			_getch();
+			system("cls");
+			temp = 0;
+			break;
+		case '4': //Удалить
+			if (List->value == 0) {
+				cout << "Список чист!" << endl << "Нажмите Любую клавишу...";
+				_getch();
+				system("cls");
+				break;
+			}
+			cout << "Введите элемент=";
+			cin >> temp;
+			if (!findFirst(List, temp)) {
+				cout << "Элемента нет!" << endl;
+				cout << "Нажмите Любую клавишу...";
+				_getch();
+				system("cls");
+				break;
+			}
+			deleteElement(List, findFirst(List, temp));
+			cout << "Элемент удалён!" << endl;
+			cout << "Нажмите Любую клавишу...";
+			_getch();
+			system("cls");
+			temp = 0;
+			break;
+		case '5': //Поиск
+			cout << "Введите элемент=";
+			cin >> temp;
+			if (findFirst(List, temp)) cout << "Первый найденный элемент: " << findFirst(List, temp);
+			else cout << "Элементов не найденно!";
+			cout << endl << "Нажмите Любую клавишу...";
+			_getch();
+			system("cls");
+			temp = 0;
+			break;
+		case '6': //Сортировка
+			sortList(List);
+			cout << "Сортировка завершена!" << endl;
+			cout << "Нажмите Любую клавишу...";
+			_getch();
+			system("cls");
+			break;
+		case '7': //Очистка
+			for (int i = List->value; i > 0; --i) deleteElement(List, 1);
+			cout << "Лист очищен!" << endl;
+			cout << "Нажмите Любую клавишу...";
+			_getch();
+			system("cls");
+			break;
+		default:
+			system("cls");
+			break;
+		}
+	}
+}
